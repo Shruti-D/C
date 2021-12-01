61d29283a24ba9c6d549d850efce56ffb78237f6
Shruti-D <65996050+Shruti-D@users.noreply.github.com>
2021-12-01 11:56:06 +0530

    Create Program9.C

---
+++ Yashvant Kanetkar Exercises/Ch2/Program9.C
@@ -0,0 +1,35 @@
+/*C program to compute Smallest number of notes that will combine to 
+  give Rs. amount*/
+#include<stdio.h>
+
+int main( )
+{
+    int amount, nohun, nofifty, noten, nofive, notwo, noone, totalnotes ;
+    
+    printf ( "Enter the amount: " ) ;
+    scanf ( "%d", &amount ) ;
+    
+    nohun = amount / 100 ;
+    amount = amount % 100 ;
+    
+    nofifty = amount / 50 ;
+    amount = amount % 50 ;
+    
+    noten = amount / 10 ;
+    amount = amount % 10 ;
+    
+    nofive = amount / 5 ;
+    amount = amount % 5 ;
+    
+    notwo = amount / 2 ;
+    amount = amount % 2 ;
+    
+    noone = amount / 1 ;
+    amount = amount % 1 ;
+    
+    totalnotes = nohun + nofifty + noten + nofive + notwo + noone ;
+    
+    printf ( "Smallest number of notes = %d\n", totalnotes ) ;
+    
+    return 0 ;
+}
